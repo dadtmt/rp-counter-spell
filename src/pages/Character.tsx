21c8f03b8e4d5bad7bcb0f1148b5a8b23f355f8e
dadtmt <thomas.culdaut@gmail.com>
2022-10-08 18:07:29 +0200

    getCharacter

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,5 +1,23 @@
+import { Alert, Loader } from '@mantine/core';
+import { useParams } from 'react-router-dom';
+import { useGetCharacterQuery } from '../utils/__generated__/graphql';
+
 const Character = () => {
-  return <div>Character</div>;
+  const { characterId } = useParams();
+  const { data, loading } = useGetCharacterQuery({
+    variables: { id: parseInt(characterId || '') },
+  });
+  const character = data?.characters_by_pk;
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!character) {
+    return <Alert>404 - character not found</Alert>;
+  }
+  const { name } = character;
+  return loading ? <Loader /> : <div>{name}</div>;
 };
 
 export default Character;
