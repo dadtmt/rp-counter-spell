9469ecb23235c881f0579c468b3eb22d344f4615
dadtmt <thomas.culdaut@gmail.com>
2022-10-16 15:33:36 +0200

    style CreateCounter

--- src/components/CreateCounter.tsx
+++ src/components/CreateCounter.tsx
@@ -1,4 +1,11 @@
-import { Button, Input, Loader, NumberInput } from '@mantine/core';
+import {
+  Button,
+  Group,
+  Input,
+  Loader,
+  NumberInput,
+  TextInput,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { showNotification } from '@mantine/notifications';
 import { useCreateCounterMutation } from '../utils/__generated__/graphql';
@@ -7,7 +14,7 @@ const CreateCounter = ({ characterId }: { characterId: number }) => {
   const [mutateCreateCounter, { loading }] = useCreateCounterMutation({
     refetchQueries: ['getCharacter'],
   });
-  const form = useForm({ initialValues: { name: '', initialValue: 0 } });
+  const form = useForm({ initialValues: { name: '', initialValue: 1 } });
   const handleSubmit = async ({ name, initialValue }: typeof form.values) => {
     try {
       await mutateCreateCounter({
@@ -24,11 +31,22 @@ const CreateCounter = ({ characterId }: { characterId: number }) => {
   };
   return (
     <form onSubmit={form.onSubmit(handleSubmit)}>
-      <Input placeholder="New counter name" {...form.getInputProps('name')} />
-      <NumberInput {...form.getInputProps('initialValue')} />
-      <Button type="submit" disabled={loading}>
-        {loading ? <Loader /> : 'Create a counter'}
-      </Button>
+      <Group position="center">
+        <TextInput
+          label="New counter name"
+          placeholder="name"
+          {...form.getInputProps('name')}
+        />
+        <NumberInput
+          label="Maximum value"
+          {...form.getInputProps('initialValue')}
+        />
+      </Group>
+      <Group position="center" mt="lg">
+        <Button type="submit" disabled={loading}>
+          {loading ? <Loader /> : 'Create a counter'}
+        </Button>
+      </Group>
     </form>
   );
 };
